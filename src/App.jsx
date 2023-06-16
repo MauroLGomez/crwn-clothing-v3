950aa002c69234bece856a08f2a77dd5d3dafce0
MauroLGomez <piberandom777@gmail.com>
2023-06-15 23:00:04 -0300

    Sign-in component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navigation from "./components/routes/navigation/navigation.component";
 
 import Home from "./components/routes/home/home.component";
 
+import SignIn from "./components/routes/sign-in/sign-in.component";
+
 const Shop = () => {
   return <h1>I am the shop page</h1>
 };
@@ -16,6 +18,7 @@ const App = () => {
       <Route path="/" element={ <Navigation /> }>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
     </>
