ec1b6e591d78adb286a42cd77884d735186018ed
MauroLGomez <piberandom777@gmail.com>
2023-08-12 00:30:52 -0300

    Changed the sign up component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navigation from "./components/routes/navigation/navigation.component";
 
 import Home from "./components/routes/home/home.component";
 
-import SignIn from "./components/routes/sign-in/sign-in.component";
+import Authentication from "./components/routes/authentication/authentication.component";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
@@ -18,7 +18,7 @@ const App = () => {
       <Route path="/" element={ <Navigation /> }>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
-        <Route path="sign-in" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
     </>
